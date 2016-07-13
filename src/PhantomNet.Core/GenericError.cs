aee3a1fa1ef1089c3f0f4bbebb76bae166dde234
mnguyen284 <mnguyen284@gmail.com>
2016-07-13 16:26:41 +0700

    Add build scripts

--- src/PhantomNet.Core/GenericError.cs
+++ src/PhantomNet.Core/GenericError.cs
@@ -2,6 +2,14 @@
 {
     public class GenericError
     {
+        public GenericError() { }
+
+        public GenericError(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
         public string Code { get; set; }
 
         public string Description { get; set; }
