2c20cb17debdd99a2579be5ad4e799c575e33950
Minh Nguyen <mnguyen284@gmail.com>
2016-07-23 18:49:57 +0700

    Pascal / Camel cases

---
+++ src/PhantomNet.Core/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace PhantomNet
+{
+    public static class StringExtensions
+    {
+        public static string ToPascalCase(this string source)
+        {
+            if (source == null)
+            {
+                return source;
+            }
+
+            if (source.Length == 1)
+            {
+                return source.ToUpper();
+            }
+
+            var words = source.Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
+                              .Select(x => $"{x.Substring(0, 1).ToUpper()}{x.Substring(1)}")
+                              .ToArray();
+
+            return string.Join(string.Empty, words);
+        }
+
+        public static string ToCamelCase(this string source)
+        {
+            if (source == null)
+            {
+                return source;
+            }
+
+            if (source.Length == 1)
+            {
+                return source.ToLower();
+            }
+
+            var words = source.Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
+                              .Select(x => $"{x.Substring(0, 1).ToUpper()}{x.Substring(1)}")
+                              .ToArray();
+
+            if (words.Length > 0)
+            {
+                words[0] = words[0].ToLower();
+            }
+
+            return string.Join(string.Empty, words);
+        }
+    }
+}
