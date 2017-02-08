c07cf21a669760f4fd6ed46ecfcffb6bdf2e95a8
Minh Nguyen <mnguyen284@gmail.com>
2017-02-08 10:12:57 +0700

    ConcurrencyStamp string length

--- src/PhantomNet.ComponentModel.Annotations/StringLengths.cs
+++ src/PhantomNet.ComponentModel.Annotations/StringLengths.cs
@@ -2,6 +2,15 @@
 {
     public static class StringLengths
     {
+        public const int TitleContent = 256;
+        public const int TinyContent = 200;
+        public const int ShortContent = 500;
+        public const int MediumContent = 1000;
+        public const int LongContent = 2000;
+        public const int MaxContent = 4000;
+
+        public const int ConcurrencyStamp = 36;
+
         public const int PersonTitle = 10;
         public const int FullName = 50;
         public const int CompanyName = 200;
@@ -9,13 +18,6 @@
         public const int PhoneNumber = 20;
         public const int Address = 200;
         public const int Website = 253;
-
-        public const int TitleContent = 256;
-        public const int TinyContent = 200;
-        public const int ShortContent = 500;
-        public const int MediumContent = 1000;
-        public const int LongContent = 2000;
-        public const int MaxContent = 4000;
 
         public const int CreditCardNumber = 16;
         public const int CreditCardSecurityCode = 4;
